92308a89f8c7bf631c02ca715129d2a290a1c7c4
mritunjayupadhyay <mupadhyay00@gmail.com>
2024-10-31 15:53:40 +0700

    feat: add basic structure

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,8 @@
+const Sidebar = () => {
+    return (
+        <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex flex-col gap-y-4 text-white p-2 items-center"
+        >Sidebar</aside>
+    )
+}
+
+export default Sidebar;
